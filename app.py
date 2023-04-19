9f78f06e9b9e2061d14cb01c75ac8dd9710763a8
Rohit Kumar <96419185+Rohit123iitan@users.noreply.github.com>
2023-04-19 12:53:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,12 +32,12 @@ continous_features = ['NUMBER1','NUMBER2','NUMBER3']
 font_size=30
 font_color="blue"
 st.subheader('The Largest Number is :')
-if(df['NUMBER1'][0]>df['NUMBER2'][0] and df['NUMBER1'][0]>df['NUMBER3'][0]):
+if(df['NUMBER1'][0]>=df['NUMBER2'][0] and df['NUMBER1'][0]>=df['NUMBER3'][0]):
     html_str = f"""<style>p.a {{color: {font_color};font: bold {font_size}px Courier;}}</style><p class="a">{df['NUMBER1'][0]}</p>"""
     st.markdown(html_str, unsafe_allow_html=True)
-if(df['NUMBER2'][0]>df['NUMBER1'][0] and df['NUMBER2'][0]>df['NUMBER3'][0]):
+if(df['NUMBER2'][0]>=df['NUMBER1'][0] and df['NUMBER2'][0]>=df['NUMBER3'][0]):
     html_str = f"""<style>p.a {{color: {font_color};font: bold {font_size}px Courier;}}</style><p class="a">{df['NUMBER2'][0]}</p>"""
     st.markdown(html_str, unsafe_allow_html=True)
-if(df['NUMBER3'][0]>df['NUMBER2'][0] and df['NUMBER3'][0]>df['NUMBER1'][0]):
+if(df['NUMBER3'][0]>=df['NUMBER2'][0] and df['NUMBER3'][0]>=df['NUMBER1'][0]):
     html_str = f"""<style>p.a {{color: {font_color};font: bold {font_size}px Courier;}}</style><p class="a">{df['NUMBER3'][0]}</p>"""
     st.markdown(html_str, unsafe_allow_html=True)
