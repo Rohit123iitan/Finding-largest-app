80ceef4872ff8f0f9e0ceb7a696792efdce09c91
Rohit Kumar <96419185+Rohit123iitan@users.noreply.github.com>
2023-04-19 12:29:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ st.write("""
 st.header('User Input Parameters')
 
 def user_input_features():
-    Number1 = st.number_input("NUMBER1",min_value=-20000,max_value=400000 )
-    Number2 = st.number_input("NUMBER2",min_value=-20000,max_value=400000 )
-    Number3 = st.number_input("NUMBER3",min_value=-20000,max_value=400000 )
+    Number1 = st.number_input("NUMBER 1",min_value=-20000,max_value=400000 )
+    Number2 = st.number_input("NUMBER 2",min_value=-20000,max_value=400000 )
+    Number3 = st.number_input("NUMBER 3",min_value=-20000,max_value=400000 )
     
 
     data = {'NUMBER1': Number1,
@@ -33,6 +33,6 @@ st.subheader('The Largest Number is :')
 if(df['NUMBER1'][0]>df['NUMBER2'][0] and df['NUMBER1'][0]>df['NUMBER3'][0]):
     st.write(df['NUMBER1'][0])
 if(df['NUMBER2'][0]>df['NUMBER1'][0] and df['NUMBER2'][0]>df['NUMBER3'][0]):
-    st.write(df['NUMBER2'][0])
+    st.write(**:blue[df['NUMBER2'][0]]** )
 if(df['NUMBER3'][0]>df['NUMBER2'][0] and df['NUMBER3'][0]>df['NUMBER1'][0]):
     st.write(df['NUMBER3'][0])
